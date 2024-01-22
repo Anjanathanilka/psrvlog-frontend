5b99f17fc6b986fed15e6460daeb8f9ea640a272
Anjanathanilka <horagollaanjana@gmail.com>
2024-01-22 15:20:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 
+
 function App() {
+
+  const navigate = useNavigate();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to PSR vlog</h1>
+        <button className='users-button' onClick={() => navigate('/users')}>Users</button>
+
       </header>
     </div>
   );
